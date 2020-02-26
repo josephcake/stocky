a248579b3f9691522fa68ab9843fa5e3b7965a9e
Joseph <joseph8p@gmail.com>
2020-02-26 06:43:55 -0500

    pre-purchase

--- server/app.js
+++ server/app.js
@@ -19,19 +19,39 @@ app.use(cors());
 app.use(express.json({extended:false}))
 app.use("/api/userModel", userPost)//mongo User model
 app.use("/api/userEmail", userEmailGet)
-// app.use("/api/users", usersGetAll)
 
 
-
-
-app.get("/api/", (req, res) => {  
-  console.log(req.headers);  
-  fetch(
-    `https://www.alphavantage.co/query?function=${req.headers.timeselected}&symbol=${req.headers.symbol}&apikey=demo`
-  )
+app.get("/api/globalquote", (req, res)=>{
+  let uri = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${req.headers.symbol}&apikey=0BSNGA5PV8BOVQ5W`
+  if (req.headers.symbol === "demo") {
+    uri = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo"
+  }
+  fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${req.headers.symbol}&apikey=0BSNGA5PV8BOVQ5W`)
     .then(res => {
       if (res.status >= 400) {
         throw new Error("Bad response from server");
+      }
+      return res.json();
+    })
+    .then(response => {
+      res.send(response);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+})
+
+app.get("/api/", (req, res) => {  
+  console.log(req.headers);  
+  let uri = `https://www.alphavantage.co/query?function=${req.headers.timeselected}&symbol=${req.headers.symbol}${req.headers.timeselected === "TIME_SERIES_INTRADAY" ? "&interval=15min" : ''}&apikey=0BSNGA5PV8BOVQ5W`
+  if(req.headers.symbol === "demo"){
+    uri = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=MSFT&apikey=demo"
+  }
+  fetch(uri)
+    .then(res => {
+      if (res.status >= 400) {
+        return res.json()
+        // throw new Error("Bad response from server");
       }
       return res.json();
     })
@@ -46,11 +66,12 @@ app.get("/api/", (req, res) => {
 app.get("/api/symbol", (req, res) => {
   const symbol = req.headers.symbol;  
   fetch(
-    `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${symbol}&apikey=demo`
+    `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${symbol}&apikey=0BSNGA5PV8BOVQ5W`
   )
     .then(res => {
       if (res.status >= 400) {
-        throw new Error("Bad response from server");
+        // throw new Error("Bad response from server");        
+        return res.json();
       }
       return res.json();
     })
